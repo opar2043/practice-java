eb3c625de97d1e4ca01b391a2f4f51180ecc8563
opar2043 <rezonerashidopar@gmail.com>
2025-10-05 11:28:05 +0600

    Bank account software created

---
+++ BankAccount.java
@@ -0,0 +1,54 @@
+public class BankAccount {
+    private String accountname;
+    private  int accountnumber ;
+    private int balance;
+
+    BankAccount(String accountname , int accountnumber , int balance){
+        this.accountname = accountname;
+        this.accountnumber = accountnumber;
+        this.balance = balance;
+        System.out.println("Bank account created for " + accountname + " (Acc Number: " + accountnumber + ")");
+
+        showBalance();
+    }
+
+    void deposit(int amount){
+        if(amount > 0){
+            System.out.println("Depositing " + amount);
+            balance = balance + amount;
+        }else{
+            System.out.println("Invalid amount to deposit.");
+        }
+
+        showBalance();
+    }
+
+
+    void withdraw(int amount){
+        if(amount> 0 && balance >= amount){
+            balance = balance - amount;
+            System.out.println("Withdrawing " + amount);
+        }else{
+            System.out.println("Invalid amount to withdraw.");
+        }
+
+        showBalance();
+    }
+
+    void showBalance(){
+        System.out.println("Current balance: " + balance);
+    }
+
+
+  public static void main(String[] args){
+    BankAccount account = new BankAccount("Rijoan Rashid" , 2034 , 5000);
+    account.deposit((2000));
+    account.withdraw(1000);
+
+
+    BankAccount account2 = new BankAccount("John Doe" , 2035 , 3000);
+    account2.deposit(700);
+  } 
+
+
+}
